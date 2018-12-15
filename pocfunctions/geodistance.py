cfaed7513e9f4bb59963d19bbb938863f181c100
Aldo Eisma <aeisma@users.noreply.github.com>
2018-12-15 17:52:36 +0100

    Added if for slicing error.

--- pocfunctions/geodistance.py
+++ pocfunctions/geodistance.py
@@ -86,10 +86,11 @@ class CalculateGeoDistanceOver(BaseTransformer):
     # AS data frame requires a window > schedule interval.
     def _calc(self, df):
         df = df.copy()
-        lat = df[self.input_item_lat][1:]
-        prev_lat = df[self.input_item_lat].shift()[1:]
-        lon = df[self.input_item_lon][1:]
-        prev_lon = df[self.input_item_lon].shift()[1:]
-        latlon = pd.DataFrame(data={self.input_item_lat: lat, self.input_item_lon: lon, self.input_item_prev_lat: prev_lat, self.input_item_prev_lon: prev_lon}, index=df.index[1:])
-        df[self.output_item] = latlon.apply(self._calc_dist, axis=1)
+        if (df.index.size > 1):
+            lat = df[self.input_item_lat][1:]
+            prev_lat = df[self.input_item_lat].shift()[1:]
+            lon = df[self.input_item_lon][1:]
+            prev_lon = df[self.input_item_lon].shift()[1:]
+            latlon = pd.DataFrame(data={self.input_item_lat: lat, self.input_item_lon: lon, self.input_item_prev_lat: prev_lat, self.input_item_prev_lon: prev_lon}, index=df.index[1:])
+            df[self.output_item] = latlon.apply(self._calc_dist, axis=1)
         return df
