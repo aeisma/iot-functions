54b9b78e4c3a62bc2e245a276539b6613ff05afc
Aldo Eisma <aeisma@users.noreply.github.com>
2018-12-15 16:38:46 +0100

    Create dataframe with same index.

--- pocfunctions/geodistance.py
+++ pocfunctions/geodistance.py
@@ -87,9 +87,9 @@ class CalculateGeoDistanceOver(BaseTransformer):
     def _calc(self, df):
         df = df.copy()
         lat = df[self.input_item_lat][1:]
-        prev_lat = df[self.input_item_lat].shift()[1:].rename(self.input_item_prev_lat)
+        prev_lat = df[self.input_item_lat].shift()[1:]
         lon = df[self.input_item_lon][1:]
-        prev_lon = df[self.input_item_lon].shift()[1:].rename(self.input_item_prev_lon)
-        latlon = pd.concat([lat, lon, prev_lat, prev_lon], axis=1)
+        prev_lon = df[self.input_item_lon].shift()[1:]
+        latlon = pd.DataFrame(data={self.input_item_lat: lat, self.input_item_lon: lon, self.input_item_prev_lat: prev_lat, self.input_item_prev_lon: prev_lon}, index=df.index[1:])
         df[self.output_item] = latlon.apply(self._calc_dist, axis=1)
         return df
