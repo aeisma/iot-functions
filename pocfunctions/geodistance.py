d87d6530d5a1bc10e913790df507cf82a8e44981
Aldo Eisma <aeisma@users.noreply.github.com>
2019-01-18 08:53:05 +0100

    Added exception handler

--- pocfunctions/geodistance.py
+++ pocfunctions/geodistance.py
@@ -46,7 +46,11 @@ class CalculateGeoDistance(BaseTransformer):
 
     def execute(self, df):
         df = df.copy()
-        df[self.output_item] = df.apply(self._calc_dist, axis=1)
+        try:
+            df[self.output_item] = df.apply(self._calc_dist, axis=1)
+        except Exception as e:
+            print(df)
+            print(e)
         return df
 
 class CalculateGeoDistanceOver(BaseTransformer):
@@ -87,10 +91,14 @@ class CalculateGeoDistanceOver(BaseTransformer):
     def _calc(self, df):
         df = df.copy()
         if (df.index.size > 1):
-            lat = df[self.input_item_lat][1:]
-            prev_lat = df[self.input_item_lat].shift()[1:]
-            lon = df[self.input_item_lon][1:]
-            prev_lon = df[self.input_item_lon].shift()[1:]
-            latlon = pd.DataFrame(data={self.input_item_lat: lat, self.input_item_lon: lon, self.input_item_prev_lat: prev_lat, self.input_item_prev_lon: prev_lon}, index=df.index[1:])
-            df[self.output_item] = latlon.apply(self._calc_dist, axis=1)
+            try:
+                lat = df[self.input_item_lat][1:]
+                prev_lat = df[self.input_item_lat].shift()[1:]
+                lon = df[self.input_item_lon][1:]
+                prev_lon = df[self.input_item_lon].shift()[1:]
+                latlon = pd.DataFrame(data={self.input_item_lat: lat, self.input_item_lon: lon, self.input_item_prev_lat: prev_lat, self.input_item_prev_lon: prev_lon}, index=df.index[1:])
+                df[self.output_item] = latlon.apply(self._calc_dist, axis=1)
+            except Exception as e:
+                print(df)
+                print(e)
         return df
